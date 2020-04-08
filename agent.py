b99ac8c54112c75d33c97f82c0f0916468fe7141
BMatuszewski <bmatuszewski@gmail.com>
2020-04-08 10:10:32 +0200

    prepare models for DDPG

--- agent.py
+++ agent.py
@@ -1,11 +1,20 @@
 import numpy as np
 import random
 
+from buffer import ReplayBuffer
+from models import ActorCriticNet
+
 class Agent:
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size, action_size, buffer_size = 2**10, batch_size=4, device="cpu"):
         print("=== AGENT CREATED ===")
         self.state_size = state_size
         self.action_size = action_size
+
+        self.network = ActorCriticNet(state_size, action_size)
+        self.target_network = ActorCriticNet(state_size, action_size)
+        self.target_network.load_state_dict(self.network.state_dict())
+
+        self.replay = ReplayBuffer(action_size,buffer_size,batch_size,0,device)
 
     def act(self, state):
         return np.random.random(self.action_size)
---
+++ models.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ActorNet(nn.Module):
+    def __init__(self, state_size, action_size, fc1_units=64, fc2_units=64):
+        super(ActorNet, self).__init__()
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)     
+
+
+class CriticNet(nn.Module):
+    def __init__(self, state_size, action_size, fc1_units=64, fc2_units=64):
+        super(CriticNet, self).__init__()
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units + action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state, action):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(torch.cat([x, action], dim=1)))
+        x = self.fc3(x)
+        return x
+
+
+class ActorCriticNet(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(ActorCriticNet, self).__init__()
+        self.actor_body = ActorNet(state_size, action_size)
+        self.critic_body = CriticNet(state_size, action_size)
+
+        actor_opt = torch.optim.Adam(self.actor_body.parameters(), lr=1e-4)
+        critic_opt = torch.optim.Adam(self.critic_body.parameters(), lr=1e-3)
+
+    def forward(self, state):
+        action = torch.tanh(self.actor(state))
+        return action
